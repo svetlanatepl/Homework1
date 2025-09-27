5be2bb93a2f3a117ea1cb1bb50929a6f06467fe8
Svetlana <studsveta@mail.ru>
2025-09-27 15:47:29 +0400

    commit папки 01_lesson

---
+++ 01_lesson/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Светлана"
+print(my_name)
---
+++ 01_lesson/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def funcNum(num):
+    print(num, end='')
+
+
+funcNum(8)
+funcNum(8)
+funcNum(0)
+funcNum(0)
+funcNum(5)
+funcNum(5)
+funcNum(5)
+funcNum(3)
+funcNum(5)
+funcNum(3)
+funcNum(5)
